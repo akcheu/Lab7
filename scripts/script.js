a3fbdbaefb8966f780ff85e038af63313b254144
Allen <akcheung@ucsd.edu>
2021-05-16 15:06:18 -0700

    Check

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,9 @@
 
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
-let postNum = 1;
 
 // Make sure you register your service worker here too
+
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
     .then(response => response.json())
@@ -12,22 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
-        newPost.id = postNum;
-        postNum++;
-        newPost.addEventListener('click', () => {
-          setState({name: 'entry', id: newPost.id});
-        });
         document.querySelector('main').appendChild(newPost);
       });
     });
-});
-
-let settings = document.querySelector('header img');
-settings.addEventListener('click', () => {
-  setState({name: 'settings'});
-});
-
-let mainPage = document.querySelector('header h1');
-mainPage.addEventListener('click', () => {
-  setState({name: 'mainPage'});
 });
